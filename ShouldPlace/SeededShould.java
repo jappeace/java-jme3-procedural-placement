7bf7a343b9788c0d13595ee74665f6ea946d592e
jappeace <jappieklooster@hotmail.com>
2013-06-30 14:37:46 +0200

    seeded should now also works with negatives, by becoming huge positives

--- ShouldPlace/SeededShould.java
+++ ShouldPlace/SeededShould.java
@@ -6,6 +6,7 @@ package nl.jappieklooster.JME3.ProceduralPlacement.ShouldPlace;
 
 import com.jme3.math.Vector3f;
 import nl.jappieklooster.JME3.ProceduralPlacement.TerrainDataAcces;
+import nl.jappieklooster.vikingr.Engine.Log;
 
 /**
  *
@@ -13,9 +14,12 @@ import nl.jappieklooster.JME3.ProceduralPlacement.TerrainDataAcces;
  */
 public class SeededShould extends ShouldPlace implements IShouldPlace{
     private String _seed;
-    private static final int  RESET_VALUE = 0;
+    
+    private static final int  RESET_VALUE = 0,  
+            SKIP_THRESHOLD = 4;
+    private static final double INCREMENT_STEPS = Double.MAX_VALUE/13;
     private static final float PER = 1500;
-    private int _placeCycle = RESET_VALUE, places = RESET_VALUE;
+    private int _placeCycle = RESET_VALUE, _places = RESET_VALUE, _skipped = RESET_VALUE;
     public SeededShould(String seed){
         _seed = seed;
     }
@@ -25,17 +29,40 @@ public class SeededShould extends ShouldPlace implements IShouldPlace{
             return false;
         }
         _placeCycle = RESET_VALUE;
-        places++;
-        boolean result = determinPlacement(where);
-        return result;
+        _places++;
+        return determinPlacement(where);
     }
     
     private boolean determinPlacement(Vector3f where){
-        return getCell().hashCode()*where.hashCode()* places < (_seed.hashCode() * where.x  + where.y * where.z );
+        if(_places % SKIP_THRESHOLD == 0){
+            // first one gets skipped, then 1 2th then 1 2 3th etc
+            _skipped++;
+            return false;
+        }
+        double smaller = 0, bigger = 0, seed = becomePositive(_seed.hashCode());
+        Log.debug("seed val: + {0}, max int: {1}", seed, Integer.MAX_VALUE);
+        // the increment steps are quite big, but this code has to run fast. One go is probably enough
+        for(double compare = RESET_VALUE; compare < seed; compare += INCREMENT_STEPS){
+            
+            smaller += _skipped*getCell().hashCode()*where.hashCode()* _places * getCell().x*where.hashCode()*where.hashCode();
+            bigger += (seed * where.x * getCell().z* getCell().z  + seed +
+                    where.y * where.z - 
+                    getCell().y );
+        }
+        return smaller < bigger;
+    }
+    /**
+     * if a number is negative it gets multiplied by 13 until overflow oceurs making it very positive
+     * @param input
+     * @return a positive value
+     */
+    private double becomePositive(double input){
+        return (input < 0)? becomePositive(input* input) : input;
     }
     @Override
     public void onFinishedPlacing() {
+        _skipped = RESET_VALUE;
         _placeCycle = RESET_VALUE;
-        places = RESET_VALUE;
+        _places = RESET_VALUE;
     }
 }
